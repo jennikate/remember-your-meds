7f59a567572d1d5846fab01e2d7101014546aa74
JenniKate Wallace <jennikate@gmail.com>
2020-01-16 11:51:45 +0000

    config

--- manage.py
+++ manage.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
+
 import dotenv
 
 
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+if __name__ == "__main__":
+    dotenv.read_dotenv()
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,8 +18,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    dotenv.read_dotenv()
-    main()
